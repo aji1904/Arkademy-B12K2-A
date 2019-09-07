cae1f690b23b05106355efdabe46c92d8a4e15b2
aji1904 <aji19980419@gmail.com>
2019-09-07 04:08:12 -0700

    edit soal 4

--- arkademy soal  4.js
+++ arkademy soal  4.js
@@ -1,7 +1,16 @@
-const bersih = input => {
-    let bersih = input.match(/[a-zA-Z0-9]/g).join('');
-    bersih = bersih.split(/(?<=\\G.{3})/)
-    console.log(bersih)
+const groupNumber = input => {
+    const result = String(input)
+        .match(/[a-zA-Z0-9]/g)
+        .join('')
+        .split(/(.{3})/)
+        .filter(res => res.length > 0)
+    
+    if (result[result.length - 1].length < 2) {
+        result[result.length - 1] = result[result.length - 2].charAt(result[result.length - 2].length - 1) + result[result.length - 1]
+        result[result.length - 2] = result[result.length - 2].slice(0, result[result.length - 2].length - 1)
+    }
+    
+    return result.join('-')
 }
 
-bersih('993141 -1 1323 14-232')
+console.log(groupNumber('993141 -1 1323 14-232'))
